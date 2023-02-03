e9afe120868c14574d52c27b7843b81c08394850
John Bynum <iexclusiveproductions@gmail.com>
2023-02-03 18:47:51 -0500

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'; 
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'; 
-// import Button from '@mui/material/Button';
+// import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'; 
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Grow from '@mui/material/Grow';
+import Grid from '@mui/material/Grid';
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from "./images/memories.png";
+import useStyles from './styles';
+
 const App = () => {
-   return (
+    const classes = useStyles();
+
+    return (
         <Container maxwidth="lg">
-           <AppBar position="static" color="inherit">
-                <Typography variant='h2' align='center'>Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+           <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar> 
             <Grow in>
                 <Container>
